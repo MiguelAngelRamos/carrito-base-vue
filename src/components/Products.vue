e41382042f18541019b40e98ff48fccbe8d04080
Miguel Angel Ramos <return.infox@gmail.com>
2024-05-06 10:32:15 -0400

    estilos de las card terminado

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -2,26 +2,36 @@
   <div class="container">
 
     <input v-model="searchQuery" class="mt-5 form-control" placeholder="Buscar Productos" />
-
-    <p>Lo existe dentro de la Instancia Vue: {{ searchQuery }}</p>
-    <p>Lo existe dentro de la Instancia Vue: {{ searchQuery.length }}</p>
-
-    <ul>
+    <div v-if="products.length === 0">Cargando productos...</div>
+    <div class="row mt-3">
       <template v-for="product in filterProduct" :key="product.id">
-        <li v-if="product && product.stock > 0" :key="product.id">
-          Nombre: {{ product.nombre }} - Precio: ${{ product.precio }} - Stock: {{ product.stock }}
+        <!-- los productos con stock superior a cero, son los productos que necesitamos renderizar  -->
+        <div v-if="product && product.stock > 0" :key="product.id" class="col-sm-6 col-md-4 col-lg-3 mb-4">
+          <div class="card h-100">
+            <img v-bind:src="product.image" class="card-img-top" alt="producto">
+            <div class="card-body d-flex flex-column">
+              <h5 class="card-title">{{ product.nombre }}</h5>
+              <p class="card-text">{{ product.precio }}</p>
+              <div class="mt-auto">
+                <div class="input-group mb-3">
+                  <!-- Estos botones son para aumentar la cantidad o disminuir la cantidad -->
+                  <button class="btn btn-outline-danger">-</button>
+                  <input class="form-control text-center" type="text">
+                  <button class="btn btn-outline-success">+</button>
+                </div>
 
-          <button
-            @click="addCart(product)" 
-            type="button" 
-            class="btn btn-outline-dark"
-            style="--bs-btn-padding-y: .15rem; --bs-btn-padding-x: .3rem; --bs-btn-font-size: .45rem;"> 
-          Agregar carrito
-          </button>
-        </li>
+                <button class="btn btn-success w-100 mb-2">Añadir al carrito</button>
+                <button class="btn btn-dark w-100">Ver el detalle</button>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
+    </div>
 
-    </ul>
+
   </div>
 </template>
 
@@ -80,4 +90,11 @@ export default {
    F && F = F
  
  -->
-<style></style>
+<style scoped>
+img {
+  width: 250px;
+  height: 350px;
+  object-fit: contain;
+  margin: 0 auto;
+}
+</style>
